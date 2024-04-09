b97713e6e7a5bcb75ddcc23590df6a151f38a865
Sophie McAlavey <sophie.mcalavey@mycit.ie>
2024-04-09 23:22:34 +0100

    Added the menu for making a booking and included the menu in which the user chooses a time slot, along with appending the list booking info for convienience writing the file further on

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
 #Project part1 Sophie Mc Alavey R00243889
+#Note - I realized after writing 106 lines of code that none of my git commits  had been pushed to the repository. Therefore, I had to create a new file on my laptop.
+
 print("ONE DAY FILM FESTIVAL")
 print("===" * 15)
 
@@ -9,3 +11,17 @@ def menu():
         print("3: Exit")
         menu_output = int(input("==> "))
         return menu_output 
+
+def making_a_booking(menu_output):
+    if menu_output == 1:
+        booking_info = []
+        print("BOOKING")
+        print("===" * 15)
+        name = input("Enter your name => ")
+        booking_info.append(name)
+        print("Choose the time slot")
+        print("1: All Day")
+        print("2: Morning")
+        print("3: Afternoon")
+        print("4: Evening")
+        chosen_time = int(input(" => "))
